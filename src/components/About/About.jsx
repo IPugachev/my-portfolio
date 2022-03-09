e8821b15aec1acdfba0b4aac2064fd200275380a
Ilya Pugachev <pi1691a@gmail.com>
2022-03-10 01:49:36 +0800

    set about experience portfolio services components

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,8 +1,50 @@
 import React from 'react'
 import './about.scss'
+import ME from '../../assets/89eBM2Zpa-M.jpg'
+import { FaAward } from 'react-icons/fa'
+import { FiUsers } from 'react-icons/fi'
+import { AiOutlineProject } from 'react-icons/ai'
 
 const About = () => {
-  return <section id='about'>About</section>
+  return (
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+      <div className='container about__container'>
+        <div className='about__me'>
+          <div className='about__me-img'>
+            <img src={ME} alt='AboutImage' />
+          </div>
+        </div>
+        <div className='about__content'>
+          <div className='about__cards'>
+            <article className='about__card'>
+              <FaAward className='about__icon' />
+              <h5>Experience</h5>
+              <small>3+ Years Working</small>
+            </article>
+            <article className='about__card'>
+              <FiUsers className='about__icon' />
+              <h5>Clients</h5>
+              <small>200+ Worldwide</small>
+            </article>
+            <article className='about__card'>
+              <AiOutlineProject className='about__icon' />
+              <h5>Projects</h5>
+              <small>50+ Completed</small>
+            </article>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus fugit eos error nemo, quidem dicta quia
+            nam, non dolore culpa nesciunt facere reprehenderit ducimus officia alias, qui veritatis eius. Id.
+          </p>
+          <a href='#contacts' className='button button-primary'>
+            Let's Talk
+          </a>
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default About
--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,8 +1,154 @@
 import React from 'react'
 import './portfolio.scss'
+import IMG1 from '../../assets/sample.jpg'
+import IMG2 from '../../assets/sample.jpg'
+import IMG3 from '../../assets/sample.jpg'
+import IMG4 from '../../assets/sample.jpg'
+import IMG5 from '../../assets/sample.jpg'
+import IMG6 from '../../assets/sample.jpg'
 
 const Portfolio = () => {
-  return <section id='portfolio'>Portfolio</section>
+  return (
+    <section id='portfolio'>
+      <h5>Mu recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className='container portfolio__container'>
+        <article className='portfolio__item'>
+          <div className='portfolio__item-img'>
+            <img src={IMG1} alt='' />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className='portfolio__item-cta'>
+            <a
+              href='https://github.com/IPugachev/study-project-toxin'
+              className='button'
+              target='_blank'
+              rel='noreferrer'>
+              Github
+            </a>
+            <a
+              href='https://github.com/IPugachev/study-project-toxin'
+              className='button button-primary'
+              target='_blank'
+              rel='noreferrer'>
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className='portfolio__item'>
+          <div className='portfolio__item-img'>
+            <img src={IMG1} alt='' />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className='portfolio__item-cta'>
+            <a
+              href='https://github.com/IPugachev/study-project-toxin'
+              className='button'
+              target='_blank'
+              rel='noreferrer'>
+              Github
+            </a>
+            <a
+              href='https://github.com/IPugachev/study-project-toxin'
+              className='button button-primary'
+              target='_blank'
+              rel='noreferrer'>
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className='portfolio__item'>
+          <div className='portfolio__item-img'>
+            <img src={IMG1} alt='' />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className='portfolio__item-cta'>
+            <a
+              href='https://github.com/IPugachev/study-project-toxin'
+              className='button'
+              target='_blank'
+              rel='noreferrer'>
+              Github
+            </a>
+            <a
+              href='https://github.com/IPugachev/study-project-toxin'
+              className='button button-primary'
+              target='_blank'
+              rel='noreferrer'>
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className='portfolio__item'>
+          <div className='portfolio__item-img'>
+            <img src={IMG1} alt='' />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className='portfolio__item-cta'>
+            <a
+              href='https://github.com/IPugachev/study-project-toxin'
+              className='button'
+              target='_blank'
+              rel='noreferrer'>
+              Github
+            </a>
+            <a
+              href='https://github.com/IPugachev/study-project-toxin'
+              className='button button-primary'
+              target='_blank'
+              rel='noreferrer'>
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className='portfolio__item'>
+          <div className='portfolio__item-img'>
+            <img src={IMG1} alt='' />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className='portfolio__item-cta'>
+            <a
+              href='https://github.com/IPugachev/study-project-toxin'
+              className='button'
+              target='_blank'
+              rel='noreferrer'>
+              Github
+            </a>
+            <a
+              href='https://github.com/IPugachev/study-project-toxin'
+              className='button button-primary'
+              target='_blank'
+              rel='noreferrer'>
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className='portfolio__item'>
+          <div className='portfolio__item-img'>
+            <img src={IMG1} alt='' />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className='portfolio__item-cta'>
+            <a
+              href='https://github.com/IPugachev/study-project-toxin'
+              className='button'
+              target='_blank'
+              rel='noreferrer'>
+              Github
+            </a>
+            <a
+              href='https://github.com/IPugachev/study-project-toxin'
+              className='button button-primary'
+              target='_blank'
+              rel='noreferrer'>
+              Live Demo
+            </a>
+          </div>
+        </article>
+      </div>
+    </section>
+  )
 }
 
 export default Portfolio
